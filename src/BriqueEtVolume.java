88e6fc0ecf731ab8cc39b21950461e39977092a6
Benjiworkzalott <79196765+Benjiworkzalott@users.noreply.github.com>
2022-05-15 15:09:22 +0200

    methode de construction terminer.

--- src/BriqueEtVolume.java
+++ src/BriqueEtVolume.java
@@ -1,28 +1,49 @@
 import java.awt.Color;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
 public class BriqueEtVolume implements Comparable{
 	public int x;
 	public int y;
+	public int z;
+	public int o;
 	public Brique brique;
-	public BriqueEtVolume(int x, int y,Brique b) {
+	public BriqueEtVolume(int x, int y,int z,int o,Brique b) {
 		this.x=x;
 		this.y=y;
+		this.z=z;
+		this.o=o;
 		this.brique=b;
 	}
 	
+	
 	public static void main(String[] args) {
-		BriqueEtVolume b= new BriqueEtVolume(1,2,new Brique("2x2", 100, 2, 2, 1, Color.red));
-		BriqueEtVolume b2= new BriqueEtVolume(1,1,new Brique("2x2", 100, 2, 2, 1, Color.red));
+		BriqueEtVolume b= new BriqueEtVolume(1,3,0,0,new Brique("3x2", 100, 2, 2, 1, Color.red));
+		BriqueEtVolume b4= new BriqueEtVolume(1,4,0,0,new Brique("4x2", 100, 2, 2, 1, Color.red));
+		BriqueEtVolume b2= new BriqueEtVolume(1,1,0,1,new Brique("1x2", 100, 2, 2, 1, Color.red));
+		BriqueEtVolume b3= new BriqueEtVolume(1,2,0,1,new Brique("2x2", 100, 2, 2, 1, Color.red));
+		PriorityQueue<BriqueEtVolume> p= new PriorityQueue<BriqueEtVolume>();
+		
+		p.add(b2);
+		p.add(b);
+		p.add(b3);
 		System.out.println(b.compareTo(b2));
-	}
-
-	public int compareTo(BriqueEtVolume o) {
-		return this.y-o.y;
+		System.out.println(p.peek().brique.nomBrique);
+		
+		PriorityQueue<BriqueEtVolume> p2=new PriorityQueue<BriqueEtVolume>(p);
+		System.out.println(p.size());
+		while(!p.isEmpty()) {
+			System.out.println(p.peek().brique.nomBrique);
+			p.poll();
+		}
+		p=p2;
+		System.out.println(p.size());
 	}
 
 	@Override
 	public int compareTo(Object o) {
-		return 0;
+		BriqueEtVolume x=(BriqueEtVolume)o;
+		return x.y-this.y;
 	}
 }
--- src/construction.java
+++ src/construction.java
@@ -17,6 +17,7 @@ public class construction extends Canvas implements Observer {
 	Brique brk = null;
 	int angle = 0;
 	double rota = 0;
+	Position1 pos=null;
 	VueDesPieces vue;
 	int dimx=25;
 	int dimy=15;
@@ -24,49 +25,209 @@ public class construction extends Canvas implements Observer {
 	PriorityQueue<BriqueEtVolume> droit;
 	PriorityQueue<BriqueEtVolume> derriere;
 	PriorityQueue<BriqueEtVolume> gauche;
-	PriorityQueue<BriqueEtVolume> dessus;
-	PriorityQueue<BriqueEtVolume> dessous;
-	PriorityQueue<BriqueEtVolume> iso;
+	Queue<BriqueEtVolume> dessus;
+	Queue<BriqueEtVolume> dessous;
+	Queue<BriqueEtVolume> iso;
 	public construction(Modele m,VueDesPieces v) {
 		super();
 		Canvas cnv = new Canvas();
 		cnv.setBackground(Color.blue);
 		cnv.setSize(1000, 600);
 		cnv.setVisible(true);
-		this.face=new PriorityQueue<BriqueEtVolume>();
-		this.droit=new PriorityQueue<BriqueEtVolume>();
-		this.derriere=new PriorityQueue<BriqueEtVolume>();
-		this.gauche=new PriorityQueue<BriqueEtVolume>();
-		this.dessus=new PriorityQueue<BriqueEtVolume>();
-		this.dessous=new PriorityQueue<BriqueEtVolume>();
-		this.iso=new PriorityQueue<BriqueEtVolume>();
+		this.face=new PriorityQueue<BriqueEtVolume>(375);
+		this.droit=new PriorityQueue<BriqueEtVolume>(375);
+		this.derriere=new PriorityQueue<BriqueEtVolume>(375);
+		this.gauche=new PriorityQueue<BriqueEtVolume>(375);
+		this.dessus=new LinkedList<BriqueEtVolume>();
+		this.dessous=new LinkedList<BriqueEtVolume>();
+		this.iso=new LinkedList<BriqueEtVolume>();
 		this.vue=v;
 		m.addObserver(this);	
 	}
 	
-	public void uneConstruction(Graphics g, Queue<BriqueEtVolume> l) {
+	public void uneConstruction(Graphics g) {
+		
 		if (angle==0) {
-			
+			this.vue.etat=1;
+			PriorityQueue<BriqueEtVolume> temp = new PriorityQueue<BriqueEtVolume>(this.face);
+			while(!this.face.isEmpty()) {
+				this.vue.sizeX=this.face.peek().x;
+				this.vue.sizeY=this.face.peek().y;
+				this.vue.angle=this.face.peek().o;
+				this.vue.brk=this.face.peek().brique;
+				this.vue.paint(getGraphics());
+				this.face.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.face=temp;
+			this.vue.etat=0;
 		}
 		if (angle==1) {
-			
+			this.vue.etat=1;
+			PriorityQueue<BriqueEtVolume> temp = new PriorityQueue<BriqueEtVolume>(this.droit);
+			while(!this.droit.isEmpty()) {
+				this.vue.sizeX=this.droit.peek().x;
+				this.vue.sizeY=this.droit.peek().y;
+				this.vue.angle=this.droit.peek().o;
+				this.vue.brk=this.droit.peek().brique;
+				this.vue.paint(getGraphics());
+				this.droit.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.droit=temp;
+			this.vue.etat=0;
 		}
 		if (angle==2) {
-			
+			this.vue.etat=1;
+			PriorityQueue<BriqueEtVolume> temp = new PriorityQueue<BriqueEtVolume>(this.derriere);
+			while(!this.derriere.isEmpty()) {
+				this.vue.sizeX=this.derriere.peek().x;
+				this.vue.sizeY=this.derriere.peek().y;
+				this.vue.angle=this.derriere.peek().o;
+				this.vue.brk=this.derriere.peek().brique;
+				this.vue.paint(getGraphics());
+				this.derriere.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.derriere=temp;
+			this.vue.etat=0;
 		}
 		if (angle==3) {
-			
+			this.vue.etat=1;
+			PriorityQueue<BriqueEtVolume> temp = new PriorityQueue<BriqueEtVolume>(this.gauche);
+			while(!this.gauche.isEmpty()) {
+				this.vue.sizeX=this.gauche.peek().x;
+				this.vue.sizeY=this.gauche.peek().y;
+				this.vue.angle=this.gauche.peek().o;
+				this.vue.brk=this.gauche.peek().brique;
+				this.vue.paint(getGraphics());
+				this.gauche.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.gauche=temp;
+			this.vue.etat=0;
+		}
+		if (angle==4) {
+			this.vue.etat=1;
+			Queue<BriqueEtVolume> temp = new LinkedList<BriqueEtVolume>(this.dessus);
+			while(!this.dessus.isEmpty()) {
+				this.vue.sizeX=this.dessus.peek().x;
+				this.vue.sizeY=this.dessus.peek().y;
+				this.vue.angle=this.dessus.peek().o;
+				this.vue.brk=this.dessus.peek().brique;
+				this.vue.paint(getGraphics());
+				this.dessus.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.dessus=temp;
+			this.vue.etat=0;
+		}
+		if (angle==6) {
+			this.vue.etat=1;
+			Queue<BriqueEtVolume> temp = new LinkedList<BriqueEtVolume>(this.dessous);
+			while(!this.dessous.isEmpty()) {
+				this.vue.sizeX=this.dessous.peek().x;
+				this.vue.sizeY=this.dessous.peek().y;
+				this.vue.angle=this.dessous.peek().o;
+				this.vue.brk=this.dessous.peek().brique;
+				this.vue.paint(getGraphics());
+				this.dessous.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.dessous=temp;
+			this.vue.etat=0;
+		}
+		if (angle==8) {
+			this.vue.etat=1;
+			Queue<BriqueEtVolume> temp = new LinkedList<BriqueEtVolume>(this.iso);
+			while(!this.iso.isEmpty()) {
+				this.vue.sizeX=this.iso.peek().x;
+				this.vue.sizeY=this.iso.peek().y;
+				this.vue.angle=this.iso.peek().o;
+				this.vue.brk=this.iso.peek().brique;
+				this.vue.paint(getGraphics());
+				this.iso.poll();
+			}
+			this.vue.angle=angle;
+			this.vue.sizeX=240;
+			this.vue.sizeY=500;
+			this.vue.brk=brk;
+			this.iso=temp;
+			this.vue.etat=0;
+		}
+		
+	}
+	
+	public void Sauvegarde() {
+		if (angle==0) {
+			this.face.add(new BriqueEtVolume(pos.x,pos.y,0,(int)rota,brk));
+		}
+		if (angle==1) {
+			this.droit.add(new BriqueEtVolume(pos.x,pos.y,0,(int)rota,brk));
+		}
+		if (angle==2) {
+			this.derriere.add(new BriqueEtVolume(pos.x,pos.y,0,(int)rota,brk));
+		}
+		if (angle==3) {
+			this.gauche.add(new BriqueEtVolume(pos.x,pos.y,0,(int)rota,brk));
+		}
+		if (angle==4) {
+			this.dessus.add(new BriqueEtVolume(pos.x,pos.y,0,(int)rota,brk));
+		}
+		System.out.println("face"+this.face.peek().brique.nomBrique);
+		System.out.println("droit"+this.droit.peek().brique.nomBrique);
+		System.out.println("derriere"+this.derriere.peek().brique.nomBrique);
+		System.out.println("gauche"+this.gauche.peek().brique.nomBrique);
+		System.out.println("dessus"+this.dessus.peek().brique.nomBrique);
+	}
+	
+	public boolean pasDeBrique() {
+		boolean peuxplacer=true;
+		if (angle==0) {
+			for (BriqueEtVolume briqueEtVolume : face) {
+				if(briqueEtVolume.x<=pos.x && briqueEtVolume.y<=pos.y && briqueEtVolume.x+briqueEtVolume.brique.longueurBrique>=pos.x && briqueEtVolume.y+briqueEtVolume.brique.hauteurBrique>=pos.y) {
+					peuxplacer=false;
+				}
+			}
+		}
+		if (angle==1) {
+			for (BriqueEtVolume briqueEtVolume : droit) {
+				
+			}
+		}
+		if (angle==2) {
+			for (BriqueEtVolume briqueEtVolume : derriere) {
+				
+			}
+		}
+		if (angle==3) {
+			for (BriqueEtVolume briqueEtVolume : gauche) {
+				
+			}
 		}
 		if (angle==4) {
 			
 		}
-		if (angle==6) {
-			
-		}
-		if (angle==8) {
-			
-		}
-		
+		System.out.println(peuxplacer);
+		return peuxplacer;
 	}
 	
 	public void paint(Graphics g){
@@ -130,7 +291,8 @@ public class construction extends Canvas implements Observer {
 								g.fillArc((int)(coordx*0.65),(int)(coordy*0.65),4,4,0,360);
 							}
 					}}
-		}		
+		}
+				uneConstruction(getGraphics());
 	}
 
 	@Override
@@ -140,14 +302,15 @@ public class construction extends Canvas implements Observer {
 		} catch (Exception e) {
 			try{
 				angle = (int) arg0;
+				rota= (int) arg0;
 				repaint();
 			}
 			catch (Exception e2) {
 				try {
-					Position1 pos = (Position1) arg0;
+					pos = (Position1) arg0;
 					System.out.println(pos);
 					
-					if (angle<4 && pos.x<dimx-1 && pos.y<dimy-1) {
+					if (pasDeBrique() && angle<4 && pos.y+brk.hauteurBrique<dimy && ((pos.x+brk.largeurBrique<dimx && rota%2==1) || (pos.x+brk.longueurBrique<dimx && rota%2==0))) {
 						this.vue.etat=1;
 						this.vue.sizeX=pos.x;
 						this.vue.sizeY=pos.y;
@@ -155,8 +318,9 @@ public class construction extends Canvas implements Observer {
 						this.vue.sizeX=240;
 						this.vue.sizeY=500;
 						this.vue.etat=0;
+						Sauvegarde();
 					}
-					if (angle>3 && angle<=6 && pos.x<dimx-1 && pos.y<dimx-1) {
+					if (pasDeBrique() && angle>3 && angle<6 && (pos.x+brk.largeurBrique<dimx && pos.y+brk.longueurBrique<dimx && rota%2==0 ||pos.x+brk.longueurBrique<dimx && pos.y+brk.largeurBrique<dimx && rota%2==1 )) {
 						this.vue.etat=1;
 						this.vue.sizeX=pos.x;
 						this.vue.sizeY=pos.y;
@@ -164,6 +328,7 @@ public class construction extends Canvas implements Observer {
 						this.vue.sizeX=240;
 						this.vue.sizeY=500;
 						this.vue.etat=0;
+						Sauvegarde();
 					}
 					
 				}
